ac1d6af79a3adcbc95eb2628cc385cb79e930926
Hammurabi <38507441+Hammurabi@users.noreply.github.com>
2025-02-22 08:11:25 +0100

    Update fineweb.py

--- fineweb.py
+++ fineweb.py
@@ -35,7 +35,7 @@ def modified_tokenizer():
     return extended_tokenizer
 
 # ------------------------------------------
-local_dir = "edu_fineweb10B"
+local_dir = "fineweb10B"
 remote_name = "sample-10BT"
 shard_size = int(1e8) # 100M tokens per shard, total of 100 shards
 
@@ -44,7 +44,7 @@ DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
 os.makedirs(DATA_CACHE_DIR, exist_ok=True)
 
 # download the dataset
-fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
+fw = load_dataset("HuggingFaceFW/fineweb", name=remote_name, split="train")
 
 
 
